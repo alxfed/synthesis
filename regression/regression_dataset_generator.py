32af7a45c4ebe89423ae80031b7d6118d82a4025
alxfed <alxfed@gmail.com>
2019-02-21 12:18:34 -0600

    regression dataset

--- regression/regression_dataset_generator.py
+++ regression/regression_dataset_generator.py
@@ -1,10 +1,14 @@
-import numpy as np
+"""
+Synthesis of datasets for regression.
+Note: Without a tambourine and Animal-speak.
+"""
 
+import numpy as np
 
 def create_regression_dataset(numberof_points=100,
                               numberof_variables=100,
                               model_rank=10,
-                              numberof_hidden_variables=1,
+                              numberof_other_variables=1,
                               model_bias=0.0,
                               noise=0.0,
                               coef=False,
@@ -14,18 +18,19 @@ def create_regression_dataset(numberof_points=100,
     then applyes it to an array of size numberof_points*numberof_variables
     of random Gaussian distributed points around 0 (plus and minus values).
     :param model_rank:
-    :param numberof_hidden_variables:
+    :param numberof_other_variables: dependent variables or hidden variables.
     :param model_bias: the "b" in w*X + b
     :param noise:
     :param coef:
     :param random_seed:
     :return:
     """
-    model_rank = min(numberof_variables, model_rank)
     generator = np.random.RandomState(random_seed)   # an instance of RandomState
-    underlying_model = np.zeros((numberof_variables, numberof_hidden_variables))
+    underlying_model = np.zeros((numberof_variables, numberof_other_variables))
+
+    model_rank = min(numberof_variables, model_rank)
     underlying_model[:model_rank, :] = 100 * generator.rand(
-        model_rank, numberof_hidden_variables)
+        model_rank, numberof_other_variables)
 
     X = generator.randn(numberof_points, numberof_variables)
     y = np.dot(X, underlying_model) + model_bias # X assignment?
@@ -46,7 +51,7 @@ Xa, ya, coefa = create_regression_dataset(
                 numberof_points=4,
                 numberof_variables=2,
                 model_rank=10,
-                numberof_hidden_variables=2,
+                numberof_other_variables=2,
                 model_bias=0.0,
                 noise=0.0,
                 coef=True,
